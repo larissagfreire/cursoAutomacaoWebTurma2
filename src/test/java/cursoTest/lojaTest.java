6bee2827b19f4e52b7375e9d088fa2c11dc7deb2
Larissa Gomes Freire <lari.gomes.freire@gmail.com>
2020-07-05 21:33:29 -0300

    test Xpath e CssSelector

--- src/test/java/cursoTest/lojaTest.java
+++ src/test/java/cursoTest/lojaTest.java
@@ -1,32 +1,24 @@
 package cursoTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class lojaTest {
-	WebDriver driver;
-	
-	public void abreNavegador() {
-		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://lojaexemplod.lojablindada.com/");
-	}
-	
-	public void fechaNavegador() {
-		driver.quit();
-	}
+public class lojaTest extends baseTest{
 	
 	@Test
-	public void realizaPesquisa () {
-		abreNavegador();
+	public void testPesquisaLivro () {
 		WebElement pesquisa = driver.findElement(By.id("search"));
 		pesquisa.sendKeys("fortaleza digital", Keys.ENTER);
-		fechaNavegador();
+		WebElement elTituloLivro = driver.findElement(By.xpath("//h2/a"));
+		String livro = elTituloLivro.getText();
+		Assert.assertEquals("[PRODUTO DE EXEMPLO] - Fortaleza Digital", livro);
+		WebElement elPreco = driver.findElement(By.cssSelector("#product-price-44 > span"));
+		String preco = elPreco.getText();
+		Assert.assertEquals("R$519,90", preco);
+		
 	}
 
 }
